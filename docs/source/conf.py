cb83fb13d475d18a80fc50ffce0ed98ebd689969
krypton-byte <rosid6434@gmail.com>
2024-01-20 10:44:24 +0700

    fix: rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('../..'))
+import neonize
+
 project = "neonize"
 copyright = "2024, krypton-byte"
 author = "krypton-byte"
