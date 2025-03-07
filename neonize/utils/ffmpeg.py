8282ad4fcb0016d283d49a64cf6141563da6235f
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-03-07 02:36:53 +0100

    Bug fixes and added support for ghost mentions

--- neonize/utils/ffmpeg.py
+++ neonize/utils/ffmpeg.py
@@ -228,7 +228,7 @@ class AFFmpeg:
         :return: The converted file in bytes
         :rtype: bytes
         """
-        MAX_STICKER_FILESIZE = "512000"
+        MAX_STICKER_FILESIZE = 512000
         temp = tempfile.gettempdir() + "/" + time.time().__str__() + ".webp"
         ffmpeg_command = [
             "ffmpeg",
@@ -257,7 +257,7 @@ class AFFmpeg:
                     "-preset",
                     "picture",
                     "-fs",
-                    MAX_STICKER_FILESIZE,
+                    f"{MAX_STICKER_FILESIZE}",
                     "-q:v",
                     bitrate,
                 ]
