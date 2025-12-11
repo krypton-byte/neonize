6434aff75bf49d9df5313408c85e2b9454de14fc
R? <zhanprasetyaningrat@gmail.com>
2025-12-11 07:30:02 +0700

    Bump version to 0.3.12.2

--- neonize/download.py
+++ neonize/download.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from .utils.platform import generated_name
 
-__GONEONIZE_VERSION__ = "0.3.12.1"
+__GONEONIZE_VERSION__ = "0.3.12.2"
 __GIT_RELEASE_URL__ = "https://github.com/ZhanNexus/neonize"
 
 
