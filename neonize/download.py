7b16e67711fe185fc0185c9f54066fd8766f754c
zhann <zhanprasetyaningrat@gmail.com>
2025-12-11 07:16:27 +0700

    feat :Add support for sending buttons

--- neonize/download.py
+++ neonize/download.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 
 from .utils.platform import generated_name
 
-__GONEONIZE_VERSION__ = "0.3.12"
-__GIT_RELEASE_URL__ = "https://github.com/krypton-byte/neonize"
+__GONEONIZE_VERSION__ = "0.3.12.1"
+__GIT_RELEASE_URL__ = "https://github.com/ZhanNexus/neonize"
 
 
 class UnsupportedPlatform(Exception):
