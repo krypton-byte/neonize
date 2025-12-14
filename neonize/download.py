5381f33bb8154e4b2744381772588463b61c7401
krypton-byte <pujasuara42@smk.belajar.id>
2025-12-14 11:44:49 +0700

    chore: update GIT_RELEASE_URL

--- neonize/download.py
+++ neonize/download.py
@@ -4,7 +4,7 @@ import requests
 from tqdm import tqdm
 from .utils.platform import generated_name
 __GONEONIZE_VERSION__ = '0.3.12.3'
-__GIT_RELEASE_URL__ = 'https://github.com/ZhanNexus/neonize'
+__GIT_RELEASE_URL__ = 'https://github.com/krypton-byte/neonize'
 
 class UnsupportedPlatform(Exception):
     pass
